62b0005f64d1e5ae9e79bf02140b336875f1a235
TravelingK <96756785+TravelingK@users.noreply.github.com>
2024-03-06 00:27:53 +0800

    Create trans_to_json.py

---
+++ .github/workflows/trans_to_json.py
@@ -0,0 +1,62 @@
+#coding:utf-8
+import json
+import os
+import string
+
+import re
+import sys
+
+
+
+def getdict(docname):
+    """
+    1输入文件，读取json，输出dict
+    """
+    with open(docname) as skill:
+        skillline=skill.read()
+        skilljson=json.loads(skillline)
+        return(skilljson)
+def outjson(nnskillline,docname):
+    """
+    将dict写入指定的文件
+    """
+    path=re.sub("[^/]{0,}$","",docname)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(docname,mode='+w') as skill:
+        skillline=json.dumps(nnskillline)
+        skill.write(skillline)
+
+
+def en_to_file(shuru_file,type=0):
+    mubiao_file=re.sub(".[a-z]{0,}$",".json",shuru_file)
+    mubiao_file=re.sub("gcs_master_library","gcs_master_library_en_json",mubiao_file)
+    skilljson=getdict(shuru_file)
+    newskill=[]
+    #print(skilljson)
+    nnewskill={}
+    for i in skilljson['rows']:
+        
+        newskilline={}
+        #newskilline['id']=i['id']
+        newskilline['name']=i['name']
+        newskilline['tags']=i['tags']
+        if 'specialization' in i:
+            newskilline['specialization']=i['specialization']
+        if 'defaults' in i:
+            defaultline=[]
+            for ii in i['defaults']:
+                nnewskilline={}
+                nnewskilline['type']=ii['type']
+                if 'name' in ii:
+                    nnewskilline['name']=ii['name']
+                if 'specialization' in ii:
+                    nnewskilline['specialization']=ii['specialization']
+                defaultline.append(nnewskilline)
+            newskilline['defaults']=defaultline
+        #print(newskilline)
+        nnewskill[i['id']]=newskilline
+    
+    outjson(nnewskill,mubiao_file)
+en_to_file(sys.argv[1])
+
