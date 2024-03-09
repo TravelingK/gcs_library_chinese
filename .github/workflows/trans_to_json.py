94b4890de97288e7bf7227587c9482febcf20d32
TravelingK <96756785+TravelingK@users.noreply.github.com>
2024-03-09 12:59:06 +0800

    Update trans_to_json.py

--- .github/workflows/trans_to_json.py
+++ .github/workflows/trans_to_json.py
@@ -46,6 +46,8 @@ def en_to_file(shuru_file,type=0):
         if 'notes' in i:
             newskilline['notes']=i['notes']
         if 'defaults' in i:
+            newskilline['defaults']=i['defaults']
+        if 'defaults' in i:
             defaultline=[]
             for ii in i['defaults']:
                 nnewskilline={}
